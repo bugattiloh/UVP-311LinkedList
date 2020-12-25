acf40f4bd160c69e355dea29dfa92563569cb4a9
bugattiloh <bugattiloh2000@gmail.com>
2020-12-25 17:49:02 +0300

    add git commit -m

--- UVP-311LinkedList/Program.cs
+++ UVP-311LinkedList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace UVP_311LinkedList
 {
@@ -8,25 +9,40 @@ namespace UVP_311LinkedList
         {
             var group = new UniversityGroup();
 
-            group.AddStudent(new Student("Ekaterina", "Antonova"));
-            group.AddStudent(new Student("Aphonina", "Anastasia"));
-            group.AddStudent(new Student("Bagian", "Grigory"));
-            group.AddStudent(new Student("Galaev", "Evgeniy"));
-            group.AddStudent(new Student("Gorodnikov", "Alexander"));
-            group.AddStudent(new Student("Doroshenko", "Petr"));
-            group.AddStudent(new Student("Zamanov", "Dmitry"));
-            group.AddStudent(new Student("Karpuhin", "Mihail"));
-            group.AddStudent(new Student("Kozlov", "Artem"));
-            group.AddStudent(new Student("Mashukov", "Ratmir"));
-            group.AddStudent(new Student("Medvedev", "Alexey"));
-            group.AddStudent(new Student("Melnik", "Anastasia"));
-            group.AddStudent(new Student("Nepryahina", "Alena"));
-            group.AddStudent(new Student("Novopasheniy", "Maxim"));
-            group.AddStudent(new Student("Sinitsa", "Egor"));
-            group.AddStudent(new Student("Skvortsov", "Denis"));
-            group.AddStudent(new Student("Tarberdyev", "Sergey"));
-            group.AddStudent(new Student("Tolkunov", "Dmitry"));
-            group.AddStudent(new Student("Yuhanson", "Mihail"));
+
+            if (args.Length > 0)
+            {
+                var lines = File.ReadAllLines(args[0]);
+              
+                foreach (var line in lines)
+                {
+                    group.AddStudent(new Student(line.Split(' ')[0], line.Split(' ')[1]));
+                }
+                Console.WriteLine("file was read");
+            }
+            else
+            {
+
+                group.AddStudent(new Student("Ekaterina", "Antonova"));
+                group.AddStudent(new Student("Aphonina", "Anastasia"));
+                group.AddStudent(new Student("Bagian", "Grigory"));
+                group.AddStudent(new Student("Galaev", "Evgeniy"));
+                group.AddStudent(new Student("Gorodnikov", "Alexander"));
+                group.AddStudent(new Student("Doroshenko", "Petr"));
+                group.AddStudent(new Student("Zamanov", "Dmitry"));
+                group.AddStudent(new Student("Karpuhin", "Mihail"));
+                group.AddStudent(new Student("Kozlov", "Artem"));
+                group.AddStudent(new Student("Mashukov", "Ratmir"));
+                group.AddStudent(new Student("Medvedev", "Alexey"));
+                group.AddStudent(new Student("Melnik", "Anastasia"));
+                group.AddStudent(new Student("Nepryahina", "Alena"));
+                group.AddStudent(new Student("Novopasheniy", "Maxim"));
+                group.AddStudent(new Student("Sinitsa", "Egor"));
+                group.AddStudent(new Student("Skvortsov", "Denis"));
+                group.AddStudent(new Student("Tarberdyev", "Sergey"));
+                group.AddStudent(new Student("Tolkunov", "Dmitry"));
+                group.AddStudent(new Student("Yuhanson", "Mihail"));
+            }
 
 
             Console.WriteLine(group.numberOfStudents);
@@ -43,6 +59,7 @@ namespace UVP_311LinkedList
 
             group.CleanGroup();
             Console.WriteLine(group.numberOfStudents);
+            Console.ReadKey();
         }
     }
 }
